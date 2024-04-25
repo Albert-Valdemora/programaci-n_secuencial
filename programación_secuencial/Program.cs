b0f9901fa480ef6359d051dc7ae7fa55de515856
Albert-Valdemora <albertvaldemorat@gmail.com>
2024-04-25 19:10:27 -0400

    Agregar archivos de proyecto.

---
+++ programación_secuencial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace programación_secuencial
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int num1,num2;
+            int Suma, Producto;
+
+            Console.WriteLine("Ingresa un numero");
+            Console.Write("-->");
+            num1 = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Ingresa un numero");
+            Console.Write("-->");
+            num2 = int.Parse(Console.ReadLine()!);
+
+            Suma = num1 + num2;
+            Producto = num1 * num2;
+
+            Console.WriteLine("");
+            Console.WriteLine($"La Suma de {num1} y {num2} es: {Suma}");
+            Console.WriteLine($"El Producto de {num1} y {num2} es: {Producto}");
+        }
+    }
+}
